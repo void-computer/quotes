666ff5937250badb12666258dc49aa45ab349984
void-computer <broadwayisbored@gmail.com>
2023-05-21 12:33:11 +0100

    Removing obsolete files and borowing code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect,useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+  const fetchExcuse = (excuse) => {
+    axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family</button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+      <p> {generatedExcuse} </p>
       </header>
     </div>
   );
